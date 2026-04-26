ba0852f6e9f48786e48811caf5326bb22b666dc2
Atharva Khilari <atharvkhilari29@gmail.com>
2026-04-26 17:11:20 +0530

    Edited insert file

--- sql/v2__insert_data.sql
+++ sql/v2__insert_data.sql
@@ -1,2 +1,3 @@
 INSERT INTO employees (name, role) VALUES ('Atharva', 'DevOps Engineer');
 INSERT INTO employees (name, role) VALUES ('John Doe', 'Trainee');
+INSERT INTO employees (name, role) VALUES ('John Mose', 'Admin');
